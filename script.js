3bbd954b178cfee59e71377ca9d63c2bc777583e
iammmei <safae.khess@gmail.com>
2023-06-06 11:57:06 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// onload = (event) => {
+console.log(document);
+document.title = "Modifying the DOM";
+console.log(document.title);
+document.body.style.background = "aquamarine";
+console.log(document.body.style.background);
+
+// };
