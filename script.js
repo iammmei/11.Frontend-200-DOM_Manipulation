7ae6fb226d1c38e5ec080f8de62738a770c8cac6
iammmei <safae.khess@gmail.com>
2023-06-06 14:01:12 +0200

    up

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 console.log(document);
 document.title = "Modifying the DOM";
 console.log(document.title);
-document.body.style.background = "aquamarine";
+document.body.style.background = "hotpink";
 console.log(document.body.style.background);
 
 // };
