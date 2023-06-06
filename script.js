85a9d401a1139f0c6fa4f257ceb0caf96b3a52a5
iammmei <safae.khess@gmail.com>
2023-06-06 14:25:16 +0200

    finished

--- script.js
+++ script.js
@@ -2,7 +2,13 @@
 console.log(document);
 document.title = "Modifying the DOM";
 console.log(document.title);
+
 document.body.style.background = "hotpink";
 console.log(document.body.style.background);
+
+const bodyElement = document.body.children;
+for (let i = 0; i < bodyElement.length; i++) {
+  console.log(bodyElement[i]);
+}
 
 // };
