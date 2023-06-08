d7c303c32318d3af544f16ef1f69acee701adb84
iammmei <safae.khess@gmail.com>
2023-06-08 15:29:41 +0200

    paragraph ex

--- exercice_selector/script.js
+++ exercice_selector/script.js
@@ -1,32 +1,38 @@
-//".important".attr("title", "This is an important item.");
-//console.log("title");
+// //".important".attr("title", "This is an important item.");
+// //console.log("title");
 
-let important = document.querySelectorAll(`.important`);
-//console.log(important);
+// let important = document.querySelectorAll(`.important`);
+// //console.log(important);
 
-for (let i = 0; i < important.length; i++) {
-  //using the loop to go through all the elements , length= the number of elementsso when we define important
-  important[i].setAttribute("title", "This is an important item");
-  // each element being looped through and added attribute
-  console.log(important[i]);
-  //print the information of element with index important with the set attribute title
-}
-
-//Select all the img tags and loop through them. If they have no important class, turn their display property to none
-
-// let img = document.querySelector("img");
-// let importantImage = document.querySelector("img .important ");
-
-// if (img === importantImage) {
-//   console.log(importantImage);
-// } else {
-//   display: none;
+// for (let i = 0; i < important.length; i++) {
+//   //using the loop to go through all the elements , length= the number of elementsso when we define important
+//   important[i].setAttribute("title", "This is an important item");
+//   // each element being looped through and added attribute
+//   console.log(important[i]);
+//   //print the information of element with index important with the set attribute title
 // }
-// test
-let img = document.querySelectorAll("img");
-for (let i = 0; i < img.length; i++) {
-  if (img[i].classList.contains("important") === true) {
-  } else {
-    img[i].style.display = "none";
+
+// //Select all the img tags and loop through them. If they have no important class, turn their display property to none
+
+// let img = document.querySelectorAll("img");
+// for (let i = 0; i < img.length; i++) {
+//   if (img[i].classList.contains("important") === true) {
+//   } else {
+//     img[i].style.display = "none";
+//   }
+// }
+
+// Loop through all the paragraphs and display their content in the console. If the paragraph has a class, display its classname as well
+
+let paragraph = document.querySelectorAll("p");
+let ptext = paragraph.innerText;
+console.log(ptext);
+
+// let pclass = paragraph.classList;
+
+for (let i = 0; i < paragraph.length; i++) {
+  // console.log(paragraph);
+  if (paragraph[i].classList === true) {
+    console.log(paragraph.classList);
   }
 }
