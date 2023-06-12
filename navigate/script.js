3e63d8bd5c35723e799a4565c8dfca7dcaed64f4
iammmei <safae.khess@gmail.com>
2023-06-12 12:03:44 +0200

    fin premier ex

--- navigate/script.js
+++ navigate/script.js
@@ -8,7 +8,9 @@ console.log(ol.children[3].nextElementSibling);
 
 const a = ol.children[0];
 const b = ol.children[4];
-b.appendChild(a);
+ol.insertBefore(b, a);
+
+//Insert beofre to add an element before another one
 
 // Move the <h2> of the third section in the second one and vice-versa
 // Delete the last section from the DOM, we don't need it anyways
