4bcd751af712411d6e8b743be7dfff796a3d8b9d
iammmei <safae.khess@gmail.com>
2023-06-12 12:30:46 +0200

    final

--- navigate/script.js
+++ navigate/script.js
@@ -1,16 +1,35 @@
 //Select the last child of the <ol> tag and put it at the beginning of the list
 
-const ol = document.querySelector("ol");
-const olChildren = ol.children;
+// const ol = document.querySelector("ol");
+// const olChildren = ol.children;
 
-console.log(olChildren);
-console.log(ol.children[3].nextElementSibling);
+// console.log(olChildren);
+// console.log(ol.children[3].nextElementSibling);
 
-const a = ol.children[0];
-const b = ol.children[4];
-ol.insertBefore(b, a);
-
-//Insert beofre to add an element before another one
+// const a = ol.children[0];
+// const b = ol.children[4];
+// //Insert beofre to add an element before another one
+// ol.insertBefore(b, a);
 
 // Move the <h2> of the third section in the second one and vice-versa
+
+const section = document.querySelectorAll("section");
+console.log(section);
+
+const secondS = section[1];
+const thirdS = section[2];
+
+const secondSchildren = secondS.children;
+const thirdSchildren = thirdS.children;
+console.log(secondSchildren);
+console.log(thirdSchildren);
+
+const h2 = document.querySelectorAll("h2");
+console.log(h2);
+
+Sh2 = h2[1];
+Sh3 = h2[2];
+
+Sh3.appendChild(Sh2);
+
 // Delete the last section from the DOM, we don't need it anyways
