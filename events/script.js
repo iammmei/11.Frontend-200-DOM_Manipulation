e3111baa2f5b227f50d417bad0ccddb0129746f4
iammmei <safae.khess@gmail.com>
2023-06-12 17:02:06 +0200

    THE END

--- events/script.js
+++ events/script.js
@@ -20,24 +20,56 @@ let SquareMagenta = document.querySelector(".violet");
 let SquareOrange = document.querySelector(".orange");
 
 let displayedWrapper = document.querySelector(".displayedsquare-wrapper");
+let ul = document.querySelector("ul");
 
 SquareGreen.addEventListener("click", () => {
   let newSquareGreen = document.createElement("div");
   newSquareGreen.classList.add("displayedsquare", "green");
   displayedWrapper.appendChild(newSquareGreen);
+
+  let newLi = document.createElement("li");
+
+  endTime = new Date();
+  let timeLapse = getElapsedTime();
+  let message = "Square created. Time lapse: " + timeLapse + "ms";
+  let textNode = document.createTextNode(message);
+  newLi.appendChild(textNode);
+  ul.appendChild(newLi);
 });
+
+// const clickOnSquare = (e) => {
+//     console.log(e.target.classList[1]);
+//     console.log(getElapsedTime());
+//   };
 
 SquareMagenta.addEventListener("click", () => {
   let newSquareMagenta = document.createElement("div");
   newSquareMagenta.classList.add("displayedsquare", "violet");
   displayedWrapper.appendChild(newSquareMagenta);
+  let newLi = document.createElement("li");
+
+  endTime = new Date();
+  let timeLapse = getElapsedTime();
+  let message = "Square created. Time lapse: " + timeLapse + "ms";
+  let textNode = document.createTextNode(message);
+  newLi.appendChild(textNode);
+  ul.appendChild(newLi);
 });
 
 SquareOrange.addEventListener("click", () => {
   let newSquareOrange = document.createElement("div");
   newSquareOrange.classList.add("displayedsquare", "orange");
   displayedWrapper.appendChild(newSquareOrange);
+  let newLi = document.createElement("li");
+
+  endTime = new Date();
+  let timeLapse = getElapsedTime();
+  let message = "Square created. Time lapse: " + timeLapse + "ms";
+  let textNode = document.createTextNode(message);
+  newLi.appendChild(textNode);
+  ul.appendChild(newLi);
 });
+
 //quand c une variable dans append child pas besoin de guillemets
 
 //code a executer quand on appuyer sur square green, CREATE ELEMENT crééer une variable lets new square , l'append le rajouter dans le dom + rajouter class
