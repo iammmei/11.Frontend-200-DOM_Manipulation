376ace463606b6ddca3773396512049c5c104d56
iammmei <safae.khess@gmail.com>
2023-06-15 14:46:25 +0200

    collection with img

--- collection/script.js
+++ collection/script.js
@@ -1,5 +1,6 @@
 let collection = [
   {
+    image: "mermaiddmelody.jpg",
     name: "Mermaid Melody Pichi Pichi Pitch",
     releaseDate: 2003,
     genres: ["Adventure", "Comedy", "Romance"],
@@ -9,6 +10,7 @@ let collection = [
       "As the mermaid princess of the North Pacific (one of the seven mermaid kingdoms), Lucia entrusts a magical pearl to a boy who falls overboard a ship one night. Lucia must travel to the human world to reclaim her pearl and protect the mermaid kingdoms. Using the power of music, Lucia is able to protect herself and the mermaid kingdoms from a growing evil force.",
   },
   {
+    image: "NANA.jpg",
     name: "Nana",
     releaseDate: 2006,
     genres: ["Award Winning", "Drama", "Romance"],
@@ -18,6 +20,7 @@ let collection = [
       "Two ostensibly opposite women, both named Nana, meet on a train going to Tokyo and grow inseparable. Nana Osaki, a determined and talented punk rock vocalist, dreams of becoming a successful rock star. On the other hand, the other Nana, nicknamed Hachi, is a bubbly and naive girl who falls in love easily. Despite their contrasting personalities, as they navigate the challenges of adulthood, including love, friendship, and pursuing their dreams in Tokyo.",
   },
   {
+    image:"Itazura.na.Kiss..jpg",
     name: "Itazura Na Kiss",
     releaseDate: 2008,
     genres: ["Comedy", "Romance"],
@@ -27,6 +30,7 @@ let collection = [
       "When her newly-built home is razed to the ground by an earthquake, low-achieving, clumsy, and troublesome third-year high school student Kotoko Aihara is forced to share a roof with the school's—and possibly Japan's—smartest student, Naoki Irie. Kotoko is not actually a complete stranger to Irie-kun; unfortunately, a single love letter that she tried to give him in the past has already sealed her fate as far as he is concerned. Throw in some quirky friends and a meddlesome mother, and Kotoko might not even have a snowball's chance in hell of winning the older Irie boy's heart. Yet Kotoko remains optimistic that, because she now lives in his house, her unattainable crush on the genius since the beginning of high school has never been more within reach.",
   },
   {
+    image: "kamikaekaito.jpg",
     name: "Kamikaze Kaito Jeanne",
     releaseDate: 1999,
     genres: ["Action", "Drama", "Mystery", "Romance", "Supernatural"],
@@ -36,6 +40,7 @@ let collection = [
       "A normal looking high school girl on the outside, Kusakabe Maron is actually the reincarnation of Jeanne d' Arc. With the help of the angel, Fin Fish, Maron works as the thief Jeanne at night to seal the demons that reside in pieces of artwork, preying upon the weak hearts of the owners. She is branded as a thief due to the fact that the artworks disappear after she seals the demons. One day, a new neighbor and classmate appears, as well as a rival in her night job, the thief Sinbad. With her own best friend being the detective's daughter, out to capture her and the appearance of her new rival, Maron's work is anything but easy.",
   },
   {
+    image: "cardccc.jpe",
     name: "Card Captor Sakura",
     releaseDate: 1998,
     genres: ["Adventure", "Comedy", "Drama", "Fantasy", "Romance"],
@@ -45,6 +50,7 @@ let collection = [
       "Sakura Kinomoto, a regular fourth-grade student in Tomoeda, stumbles upon a mysterious book called 'The Clow' filled with magical cards. Unintentionally, she scatters the cards throughout the town, triggering the awakening of Keroberos, also known as Kero, the Beast of the Seal. Kero informs Sakura that these cards, known as the Clow Cards, were created by the powerful sorcerer Clow Reed and had been sealed away due to their dangerous abilities. Now that the Clow Cards are free, Sakura is appointed as the Cardcaptor and entrusted with the Sealed Key by Kero. Her mission is to locate and recapture all the cards before they bring harm to the world. With the support of her best friend Tomoyo Daidouji and Kero's guidance, Sakura must balance her newfound responsibilities as the Cardcaptor with her everyday life.",
   },
   {
+    image :"vampirej.jpe",
     name: "Vampire Knight",
     releaseDate: 2008,
     genres: ["Drama", "Mystery", "Romance", "Supernatural"],
@@ -54,6 +60,7 @@ let collection = [
       "The earliest thing Yuki Cross remembers is being attacked on a snowy night by a vampire and being rescued by Kaname Kuran, who is also a vampire. The story takes place 10 years after that event. Yuki is now the adopted daughter of the Headmaster of Cross Academy. She and her longtime friend Zero Kiryu keep peace at the academy, making sure no problems arise between the Day Class (humans) and the Night Class (vampires). However, the peace they try to keep is fragile, and personal feelings begin to interrupt their duty.",
   },
   {
+    image: "doremi.jpg" ,
     name: "Magical DoReMi",
     releaseDate: 1999,
     genres: ["Comedy", "Fantasy"],
@@ -63,6 +70,7 @@ let collection = [
       "Harukaze Doremi considers herself to be the unluckiest girl in the world. Her parents are always fighting, her little sister makes fun of her, and her crush pines after another girl. If only Doremi could just wave a magic wand, she would have a much better life—or so she used to think. After a mishap with a real witch, Doremi becomes an apprentice witch herself, and it turns out she's pretty horrible at that, too. Now she and her two friends must study to become better at magic so they can become good witches and pass the witch exams to become full-fledged witches. Only then will Doremi's debt to the witch Majorika be repaid.",
   },
   {
+    image: "utena.jpg",
     name: "Princess Utena",
     releaseDate: 1997,
     genres: ["Award Winning", "Comedy", "Drama", "Fantasy", "Mystery"],
@@ -72,6 +80,7 @@ let collection = [
       "Years ago, a tragic incident befell a young princess when both her parents died. Devastated, it seemed nothing would calm this poor soul. However, a prince traveling through the area came to see the princess, hoping to cheer her up. After wiping her sorrowful tears, the prince gave her a ring carrying a rose emblem and told her as long as she holds onto the ring, they are destined to meet again. The event leaves a deep impression on the girl, Utena Tenjou, leading her to become a prince herself. Years later, Utena attends Ootori Academy, recognized by the same rose emblem as her precious ring's. There, attracted by the scent of roses, she witnesses Anthy Himemiya tending the flowers, accompanied by the Student Council President Touga Kiryuu and Vice President Kyouichi Saionji, who seem to be arguing over Anthy. Subsequently, a misunderstanding leads to Utena being dragged into the world of Duelists—those with rings similar to her own. The Duelists fight for the ownership of the Rose Bride, Anthy Himemiya, who is said to possess great power. Wanting to prove her capabilities as a prince and enraged that Anthy is being wronged and objectified, Utena resolves to fight against the Duelists to save her from the cruel fate.",
   },
   {
+    image : "fushigiyugi.jpg",
     name: "Fushigi Yûgi",
     releaseDate: 1995,
     genres: ["Adventure", "Fantasy", "Romance"],
@@ -81,6 +90,7 @@ let collection = [
       "During a visit to the National Library, Miaka Yuuki and Yui Hongo stumble upon a strange old book that casts a red light, sucking them inside its unfamiliar world. Upon arrival, the two encounter hostile slave traders and barely escape with the help of Tamahome—a powerful young man bearing a Chinese symbol on his forehead. But, a moment later, the red light returns and takes Yui away. Desperate to reunite with her companion, Miaka asks Tamahome for assistance. However, the situation escalates when the pair encounters the land's emperor, Hotohori, who believes Miaka is the foretold priestess of the kingdom's protector god Suzaku. By gathering the god's seven Celestial Warriors, the priestess can summon Suzaku and have all her wishes granted. Hotohori hopes this will save his country, and since it appears to be a fitting solution to the girl's problems as well, he convinces her to accept the role.",
   },
   {
+    image : "paradisekiss.jpg"
     name: "Paradise Kiss",
     releaseDate: 2005,
     genres: ["Drama", "Romance"],
@@ -90,6 +100,7 @@ let collection = [
       "Yukari is a typical high-school student who listens to her parents and attends school every day. As she starts to question her way of life, she encounters a group of fashion design students who have a clothing label known as 'Paradise Kiss.' The group needs to find a model to showcase their designs in an upcoming fashion show and decides to pick Yukari instead. Initially, Yukari was reluctant to be associated with this seemingly eccentric group, but eventually, she realizes that they are really nice people. Furthermore, their passion and enthusiasm to follow their ideals and dreams make Yukari realize that she has not been enjoying her life, and this motivated her to pursue her own dreams.",
   },
   {
+    image: "shugochara.jpg",
     name: "Shugo Chara!",
     releaseDate: 2009,
     genres: ["Comedy", "Fantasy", "Romance"],
@@ -101,3 +112,5 @@ let collection = [
       As Amu embraces her role, she forms the Seiyo Elementary's Guardians, a group dedicated to protecting dreams and happiness. Alongside her Shugo Charas and newfound friends, she embarks on a quest to find the X Eggs and purify them. `,
   },
 ];
+
+console.log(collection);
